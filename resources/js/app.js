e0f7c0cd54939f8a2f9187e9eb822ef367b3c605
will <will@fishpond.co.nz>
2019-05-29 21:41:29 +1200

    integrate with template

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,54 +5,26 @@
  */
 require('./bootstrap');
 
-/**
- * Next, we will create a fresh React component instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-require('./components/Example');
-
-import ReactDOM from "react-dom";
 import React from "react";
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
 import {createBrowserHistory} from "history";
-import {Router, Route, Link, Switch, Redirect} from "react-router-dom";
-import {routes} from "./components/routes";
+import {Router, Route, Switch, Redirect} from "react-router-dom";
+import ReactDOM from "react-dom";
+
+// core components
+import Admin from "./layouts/Admin.jsx";
+import RTL from "./layouts/RTL.jsx";
+
+import "./assets/css/material-dashboard-react.css?v=1.6.0";
 
 const hist = createBrowserHistory();
 
-function App() {
-    return (
-        <Router history={hist}>
-            <div style={{display: "flex"}}>
-                <div>
-                    <List className="nzg-side-nav">
-                        <ListItem button><Link to="/">Home</Link></ListItem>
-                        <ListItem button><Link to="/org">Organization</Link></ListItem>
-                        <ListItem button><span onClick={LogoutFunc}>Logout</span></ListItem>
-                    </List>
-                </div>
-                <div className="nzg-content">
-                    <Switch>
-                        {routes.map((route, index) => (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                exact={route.exact}
-                                component={route.main}
-                            />
-                        ))}
-                        <Redirect to="/dashboard"/>
-                    </Switch>
-                </div>
-            </div>
-        </Router>
-    );
-}
-
 ReactDOM.render(
-    <App/>,
-    document.getElementById('app')
+    <Router history={hist}>
+        <Switch>
+            <Route path="/admin" component={Admin}/>
+            <Route path="/rtl" component={RTL}/>
+            <Redirect from="/" to="/admin/dashboard"/>
+        </Switch>
+    </Router>,
+    document.getElementById("app")
 );
-
--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,16 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    resolve: {
+        alias: {
+            '-assets': __dirname + '/resources/js/assets',
+            '-components': __dirname + '/resources/js/components',
+            '-views': __dirname + '/resources/js/views',
+            '-variables': __dirname + '/resources/js/variables'
+        },
+    }
+});
 
 mix.react('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
